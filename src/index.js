6344c0cbd4275d17d899585b7fa3ff42a3e0dd47
L1S22BSSE0096 <maryamshahid1600@gmail.com>
2025-06-30 17:33:05 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "../styles/index.css";
+import "../styles/about.css";
+import "../styles/services.css";
+import "../styles/contact.css";
+import "../styles/blog.css";
+import "../styles/portfolio.css";
